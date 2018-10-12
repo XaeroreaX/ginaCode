e7d4d6f302d92bb25cc078473f33fbbd84244f4e
XaeroreaX <martinpitameglia@gmail.com>
2018-10-12 10:09:51 -0300

    Add files via upload

---
+++ eval01/pp_televisores_alumnos/Entidades/Entidades.ts
@@ -0,0 +1,28 @@
+
+
+export namespace Entidades
+{
+
+    export class Producto
+    {
+        private _codigo : number;
+        private _marca : string;
+        private _precio : number;
+
+        public constructor(codigo : number, marca : string, precio : number)
+        {
+            this._codigo = codigo;
+            this._marca = marca;
+            this._precio = precio;
+
+        }
+
+
+
+        
+        
+    }
+
+
+
+}
