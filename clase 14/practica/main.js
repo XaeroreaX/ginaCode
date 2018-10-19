fd56623ca27afd48131a7c111f60ec02097fff70
XaeroreaX <martinpitameglia@gmail.com>
2018-10-19 10:42:00 -0300

    Add files via upload

---
+++ clase 14/practica/main.js
@@ -0,0 +1,6 @@
+"use strict";
+/// <reference path = "./libs/jquery/index.d.ts"/>
+$(document).ready(function () {
+    var usuarios = "[{'mail' : 'gino@hotmail.com', 'clave'}]";
+});
+//# sourceMappingURL=main.js.map
---
+++ clase 14/practica/main.ts
@@ -0,0 +1,12 @@
+/// <reference path = "./libs/jquery/index.d.ts"/>
+
+declare var $ : any;
+
+
+
+$(document).ready(function(){
+   
+    var usuarios : string = "[{'mail' : 'gino@hotmail.com', 'clave'}]"
+
+
+ });
