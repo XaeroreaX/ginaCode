059ece46e3b7d524b0af90c6533d42939019bc91
XaeroreaX <martinpitameglia@gmail.com>
2018-10-26 11:44:32 -0300

    Add files via upload

---
+++ clase 16/ejercitacion/container.js
@@ -0,0 +1,6 @@
+"use strict";
+function changeColor() {
+    var algo = new HTMLButtonElement();
+    algo.getElementsBy;
+}
+//# sourceMappingURL=container.js.map
---
+++ clase 16/ejercitacion/container.ts
@@ -0,0 +1,9 @@
+
+
+function changeColor() : void
+{
+    var algo : HTMLButtonElement = new HTMLButtonElement(); 
+
+    (<HTMLButtonElement>algo).getElementsBy
+
+}
